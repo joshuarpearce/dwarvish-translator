c3f42589349640abf5da842a62e72b173f155489
Joshua Pearce <jpearce@scottlogic.com>
2021-09-09 21:49:12 +0100

    Refactor

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 
 import './App.css';
 
+const ignoredCharacters = [' '];
+
 const dwarvishCharacters = ['2', '3', '6', '8V', '8', '9', '@', '%', 'w', 'eV', 'e', 'r', 'uI,', 'a', 'd', 'f', 'g', 'l', 'll ?', ';', 'S', 'D', 'z', 'x', 'c', 'v', 'b', 'nIM', '.', '/>', 'Z ?'];
 const englishCharacters = ['b', 'f', 'm', 'th', 't', 'd', 'r', 'sh', 'z', 'kh', 'k', 'g', 'n', 'l', 'nd', 'h', 'ʔ, ‘', 'i', 'î', 'y', 'u', 'û', 'e', 'ê', 'a', 'â', 'o', 'ô', 's', 'ə', 'ʌ'];
 
@@ -11,43 +13,38 @@ const App = () => {
     const [dwarvish, setDwarvish] = useState('');
 
     const updateEnglish = (value = '') => {
-        setEnglish(value);
+        let adjustedValue = value;
 
         const valueArray = value.split('');
 
         let dwarvishTranslation = '';
 
         for (let i = 0; i < valueArray.length;) {
-            if (
-                valueArray.length - i > 2
-                && englishCharacters.includes(valueArray.slice(i, i + 3).join(''))
-            ) {
-                dwarvishTranslation += dwarvishCharacters[englishCharacters.indexOf(valueArray.slice(i, i + 3).join(''))];
-                i += 3;
+            if (ignoredCharacters.includes(valueArray[i])) {
+                dwarvishTranslation += adjustedValue[i];
+                i++;
                 continue;
             }
 
-            if (
-                valueArray.length - i > 1
-                && englishCharacters.includes(valueArray.slice(i, i + 2).join(''))
-            ) {
-                dwarvishTranslation += dwarvishCharacters[englishCharacters.indexOf(valueArray.slice(i, i + 2).join(''))];
-                i += 2;
-                continue;
-            }
+            for (let j = 3; j >= 0; j--) {
+                if (j === 0) {
+                    adjustedValue.replace(valueArray[i], '');
+                    i += 1;
+                    break;
+                }
 
-            if (
-                englishCharacters.includes(valueArray[i])
-            ) {
-                dwarvishTranslation += dwarvishCharacters[englishCharacters.indexOf(valueArray[i])];
-                i += 1;
-                continue;
-            } else {
-                i += 1;
-                continue;
+                if (
+                    valueArray.length - i > (j - 1)
+                    && englishCharacters.includes(valueArray.slice(i, i + j).join(''))
+                ) {
+                    dwarvishTranslation += dwarvishCharacters[englishCharacters.indexOf(valueArray.slice(i, i + j).join(''))];
+                    i += j;
+                    break;
+                }
             }
         }
 
+        setEnglish(adjustedValue);
         setDwarvish(dwarvishTranslation);
     };
 
---
+++ src/languageProcessor.js
@@ -0,0 +1,36 @@
+const processText = (value = '', source, destination, ignoredCharacters) => {
+    let adjustedValue = value;
+
+    const valueArray = value.split('');
+
+    let translatedValue = '';
+
+    for (let i = 0; i < valueArray.length;) {
+        if (ignoredCharacters.includes(valueArray[i])) {
+            translatedValue += adjustedValue[i];
+            i++;
+            continue;
+        }
+
+        for (let j = 3; j >= 0; j--) {
+            if (j === 0) {
+                adjustedValue.replace(valueArray[i], '');
+                i += 1;
+                break;
+            }
+
+            if (
+                valueArray.length - i > (j - 1) &&
+                source.includes(valueArray.slice(i, i + j).join(''))
+            ) {
+                translatedValue += destination[source.indexOf(valueArray.slice(i, i + j).join(''))];
+                i += j;
+                break;
+            }
+        }
+    }
+
+    return [adjustedValue, translatedValue];
+};
+
+export default processText;
